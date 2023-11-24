0b9f03e350ec5229794e8e03c0926c09d43035e1
Joaogalescky <joaocvgalescky@gmail.com>
2023-11-23 22:21:00 -0300

    update - new map and style.css

---
+++ teste1/script.js
@@ -0,0 +1,41 @@
+// Leaflet - leafletjs.com
+
+const map = L.map('map').setView([-24.9558300, -53.4552800], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-24.9558300, -53.4552800]).addTo(map)
+.bindPopup('Marcação de CSS.<br> Costumizável.')
+.openPopup('');
+
+let isFetching = false
+
+map.on("click", async e => {
+    if(isFetching) return
+
+    isFetching = true
+
+    const { lat, lng } = e.latlng
+    console.log(lat, lng)
+
+    const link = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&addressdetails=1&format=jsonv2&`
+
+    const data = await fetch(link).then(response => response.json()).catch(error => false)
+
+    console.log(data)
+
+    setTimeout(() => isFetching = false, 1000)
+})
+
+/* Links:
+//https://www.fernandoquadro.com.br/html/2019/04/18/introducao-ao-leaflet-estilos-parte-13/
+//https://github.com/dwilhelm89/Leaflet.StyleEditor --> GITHUB com os arquivos editados do Leaflet
+https://www.youtube.com/watch?v=hLd0bgVXZpc --> Utilizado
+https://www.youtube.com/watch?v=cBQcMLzq5gU --> Visualizado
+https://www.youtube.com/watch?v=v_TLg3ncUyU --> Visualizado
+https://www.youtube.com/watch?v=XK6TDA2vqD0
+https://leafletjs.com/examples/custom-icons/
+https://leafletjs.com/examples/choropleth/
+*/
